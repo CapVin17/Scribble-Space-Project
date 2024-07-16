d7abd52d1650f0f844ee08e940d87364be14cb26
CapVin17 <116640909+CapVin17@users.noreply.github.com>
2024-07-16 20:06:15 +0530

    Everything working fine uptil here

--- frontend/src/hooks/index.tsx
+++ frontend/src/hooks/index.tsx
@@ -2,10 +2,18 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { BACKEND_URL } from "../config";
 
+interface Blog{
+    title: string,
+    content: string,
+    id: string,
+    author: {
+        name: string,
+    }
+}
 
 export const useBlogs = () => {
     const [loading, setloading] = useState(true);
-    const [blogs, setblogs] = useState([]);
+    const [blogs, setblogs] = useState<Blog[]>([]);
 
     useEffect(() => {
         axios.get(`${BACKEND_URL}/api/v1/blog/bulk`,{
--- frontend/src/pages/blogs.tsx
+++ frontend/src/pages/blogs.tsx
@@ -1,29 +1,29 @@
 import { Appbar } from "../Components/appbar";
 import { useBlogs } from "../hooks";
-import { BlogsCard } from "./blogsCard"
+import { BlogsCard } from "./blogsCard";
 
 export const Blogs = () => {
+  const { loading, blogs } = useBlogs();
 
-    const {loading, blogs} = useBlogs();
+  if (loading) {
+    return <div>loading....</div>;
+  }
 
-    if(loading)
-    {
-        return <div>
-            loading....
-        </div>
-    }
-
-    return <div>
-        <Appbar />
+  return (
+    <div>
+      <Appbar />
       <div className="flex justify-center">
         <div className="max-w-xl">
-            {blogs.map(blog => <BlogsCard
-            title="Learn about Cloudfare Workers and take a very deep in-site into it that will certainly change your life"
-            content="Hi there this is vineet singh this side and i am here to tell you about one of the great technologies in use today to support your backend code and make it out to the world at very minimal cost and rather nothing it has a very strong build system and enhanced user experience"
-            publisheddate="15 July 2024"
-            authorname="Vineet Singh"
-          />)}
-        </div>
+          {blogs.map((blog) => (
+            <BlogsCard
+              title={blog.title}
+              content={blog.content}
+              publisheddate="15 July 2024"
+              authorname={blog.author.name}
+            />
+          ))}
         </div>
       </div>
-}
+    </div>
+  );
+};
